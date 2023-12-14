ae3e3ab48bf77a87663bb43d317a6332596cae7b
William Hibberd <will@williamhibberd.co.uk>
2023-12-14 15:35:56 +0000

    wip coaches data

---
+++ src/data/coaches.ts
@@ -0,0 +1,46 @@
+import type { ImageMetadata } from "astro";
+
+const coaches = [
+    {
+        name:'Amr',
+        link: 'https://fidi.opensquash.org/pros/amr-khalifa'
+    },
+    {
+        name: 'Hannah',
+        link: 'https://fidi.opensquash.org/pros/hannah-craig'
+    },
+    {
+        name: 'Ciara',
+        link: 'https://fidi.opensquash.org/pros/ciara-richards'
+    },
+    {
+        name: 'Juan',
+        link: 'https://fidi.opensquash.org/pros/juan-santos-edcec04f-f9a3-48ee-86d1-8c88740a3e01'
+    },
+    {
+        name: 'Jana',
+        link: 'https://fidi.opensquash.org/pros/jana-shiha'
+    },
+    {
+        name: 'Peter',
+        link: 'https://bryantpark.opensquash.org/pros/peter-creed-00999f78-8e86-42b2-8278-97eab718c56a'
+    },
+    {
+        name: 'David',
+        link: 'https://bryantpark.opensquash.org/pros/david-hughes-304f86e2-05c1-4f2f-9fda-774e1aa76d18'
+    },
+    {
+        name: 'Thaisa',
+        link: 'https://bryantpark.opensquash.org/pros/thaisa-serafini'
+    },
+    {
+        name: 'Asghar',
+        link: 'https://bryantpark.opensquash.org/pros/asghar-abbas'
+    },
+    {
+        name: 'Josh',
+        link: 'https://bryantpark.opensquash.org/pros/josh-easdon'
+    },
+];
+
+export default coaches;
