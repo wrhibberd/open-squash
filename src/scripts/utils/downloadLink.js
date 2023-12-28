73c908b360303dbbe7e709253bfa533634a8f53f
William Hibberd <will@williamhibberd.co.uk>
2023-12-28 15:24:29 +0000

    add view transitions

--- src/scripts/utils/downloadLink.js
+++ src/scripts/utils/downloadLink.js
@@ -1,10 +1,14 @@
-const isMacLike = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
-if (!isMacLike) {
-	setTimeout(() => {
-		const links = document.querySelectorAll("[data-download='app']");
-		links.forEach((link) => {
-			link.href =
-				"https://play.google.com/store/apps/details?id=com.playbypoint.opensquash";
-		});
-	}, 300);
-}
+const setDownloadLink = () => {
+	const isMacLike = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+	if (!isMacLike) {
+		setTimeout(() => {
+			const links = document.querySelectorAll("[data-download='app']");
+			links.forEach((link) => {
+				link.href =
+					"https://play.google.com/store/apps/details?id=com.playbypoint.opensquash";
+			});
+		}, 300);
+	}
+};
+setDownloadLink();
+document.addEventListener("astro:after-swap", setDownloadLink);
