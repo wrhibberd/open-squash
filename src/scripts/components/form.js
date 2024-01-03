f8cd7ab2e9d78b235a0a356fb53da1dfbc5f8439
William Hibberd <will@williamhibberd.co.uk>
2024-01-03 07:11:44 +0000

    test chaining form submissions

--- src/scripts/components/form.js
+++ src/scripts/components/form.js
@@ -7,32 +7,37 @@ export default function form() {
 			const form = this.$root.querySelector("form");
 			if (this.$validate.isComplete(form)) {
 				const formData = new FormData(form);
-				// console.log(formData);
-				fetch("/", {
+				const netlifyData = new URLSearchParams(formData);
+				const netlifyBody = netlifyData.toString();
+				const salesfoceData = Object.fromEntries(formData);
+				const salesforceBody = JSON.stringify(salesfoceData);
+
+				const postToNetlify = fetch("/", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/x-www-form-urlencoded",
 					},
-					body: new URLSearchParams(formData).toString(),
-				})
-					// .then(() =>
-					// 	fetch(
-					// 		"https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&orgId=00D0b000000CDCM",
-					// 		{
-					// 			method: "POST",
-					// 			headers: {
-					// 				"Content-Type":
-					// 					"application/x-www-form-urlencoded",
-					// 			},
-					// 			body: new URLSearchParams(formData).toString(),
-					// 		}
-					// 	)
-					// )
-					// .then(() => console.log("Form successfully submitted"))
+					body: netlifyBody,
+				});
+
+				const postToSalesforce = fetch(
+					"https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&orgId=00D0b000000CDCM",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: salesforceBody,
+					}
+				);
+
+				postToNetlify
+					.then(postToSalesforce)
 					.then(() => this.formSuccess())
 					.catch((error) => alert(error));
 			}
 		},
+
 		formSuccess() {
 			this.success = true;
 		},
