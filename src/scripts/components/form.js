0bea9045aaf92e27b6495aa5b9d688d3d3e6cba8
William Hibberd <will@williamhibberd.co.uk>
2024-01-11 20:03:20 +0000

    Add hidden input fields for URL parameters in form.js

--- src/scripts/components/form.js
+++ src/scripts/components/form.js
@@ -3,6 +3,68 @@ export default function form() {
 		success: false,
 		firstName: "",
 		secondName: "",
+		init() {
+			this.checkForURLParams();
+		},
+		checkForURLParams() {
+			const form = this.$root.querySelector("form");
+			const urlParams = new URLSearchParams(window.location.search);
+			const source = urlParams.get("utm_source");
+			const medium = urlParams.get("utm_medium");
+			const term = urlParams.get("utm_term");
+			const campaign = urlParams.get("utm_campaign");
+			const content = urlParams.get("utm_content");
+			if (source) {
+				// add hidden input to form
+				const sourceInput = document.createElement("input");
+				// set input attributes
+				sourceInput.setAttribute("type", "hidden");
+				sourceInput.setAttribute("name", "00N5a00000CsKs9");
+				sourceInput.setAttribute("value", source);
+				// add input to start of form
+				form.insertBefore(sourceInput, form.firstChild);
+			}
+			if (medium) {
+				// add hidden input to form
+				const mediumInput = document.createElement("input");
+				// set input attributes
+				mediumInput.setAttribute("type", "hidden");
+				mediumInput.setAttribute("name", "00N5a00000CsKsE");
+				mediumInput.setAttribute("value", medium);
+				// add input to start of form
+				form.insertBefore(mediumInput, form.firstChild);
+			}
+			if (term) {
+				// add hidden input to form
+				const termInput = document.createElement("input");
+				// set input attributes
+				termInput.setAttribute("type", "hidden");
+				termInput.setAttribute("name", "00N5a00000CsKsJ");
+				termInput.setAttribute("value", term);
+				// add input to start of form
+				form.insertBefore(termInput, form.firstChild);
+			}
+			if (campaign) {
+				// add hidden input to form
+				const campaignInput = document.createElement("input");
+				// set input attributes
+				campaignInput.setAttribute("type", "hidden");
+				campaignInput.setAttribute("name", "00N5a00000CsKsO");
+				campaignInput.setAttribute("value", campaign);
+				// add input to start of form
+				form.insertBefore(campaignInput, form.firstChild);
+			}
+			if (content) {
+				// add hidden input to form
+				const contentInput = document.createElement("input");
+				// set input attributes
+				contentInput.setAttribute("type", "hidden");
+				contentInput.setAttribute("name", "00N5a00000CsKsT");
+				contentInput.setAttribute("value", content);
+				// add input to start of form
+				form.insertBefore(contentInput, form.firstChild);
+			}
+		},
 		submitForm() {
 			const form = this.$root.querySelector("form");
 			if (this.$validate.isComplete(form)) {
