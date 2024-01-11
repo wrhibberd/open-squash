40fd21c2d4a52db63e8ebbe31c244bb43b4ea21a
William Hibberd <will@williamhibberd.co.uk>
2024-01-11 15:30:55 +0000

    Add squooshImageService to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://opensquash.org",
+	image: {
+		service: squooshImageService(),
+	},
 	integrations: [
 		tailwind({
 			config: {
