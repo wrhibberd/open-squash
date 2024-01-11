ac850f0a702428ee0f91735bee2ffaf3a6ee14a1
William Hibberd <will@williamhibberd.co.uk>
2024-01-11 16:14:47 +0000

    Update package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -6,9 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://opensquash.org",
-	image: {
-		service: squooshImageService(),
-	},
 	integrations: [
 		tailwind({
 			config: {
